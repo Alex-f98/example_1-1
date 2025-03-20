eb2b72871de776c129b39903a999adc6fff09662
Alex-f98 <bfuentes@fi.uba.ar>
2025-03-20 00:01:14 +0000

    comente cosas

--- main.cpp
+++ main.cpp
@@ -3,14 +3,23 @@
 
 int main()
 {
+    //Entrada digital para gas detector
     DigitalIn gasDetector(D2);
 
+    // Salida digital para ALARMA
     DigitalOut alarmLed(LED1);
 
+    // MODO pull dom D2
     gasDetector.mode(PullDown);
 
+    //Inicializo en OFF LED1.
+    // nose
     alarmLed = OFF;
 
+    // Las direcciones de memroia de los puertos estan en definidas en ST en un struct.
+    // hay dos struct que definer numeros y con ello se obtiene los puertos necesarios.
+    // DigitalIn.h esta en mbeg_gpio en una clase de gpio define como pines de proposito general y v inciando constructores
+    // segun los argumetos
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
