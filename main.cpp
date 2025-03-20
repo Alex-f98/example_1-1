c3dfd5c40c29edfe487cb99e5bda5ab026b0e7d0
Alex-f98 <bfuentes@fi.uba.ar>
2025-03-20 00:36:01 +0000

    Comendado un poco mas

--- main.cpp
+++ main.cpp
@@ -20,6 +20,9 @@ int main()
     // hay dos struct que definer numeros y con ello se obtiene los puertos necesarios.
     // DigitalIn.h esta en mbeg_gpio en una clase de gpio define como pines de proposito general y v inciando constructores
     // segun los argumetos
+
+    // Las definiciones de que TARGET usar esta en CMakeList.txt es una variable TARGET definida en .mbed
+    // Esa variable la modifica la IDE web y en base a eso la usa el CMake. y luego con ellos compila.
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
@@ -30,3 +33,8 @@ int main()
         }
     }
 }
+
+
+// En mbed-os estan los targets que serian las distintas placas.
+// En algun punto se tiene que definir cual de ellas usar y en base a eso
+// se decide que tipo de mapeo hacer.
